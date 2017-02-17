fa4a765ec7e0546711d9aa44e83591ee846b030c
underr <underr-ua@users.noreply.github.com>
2017-02-17 08:44:21 +0200

    demo

---
+++ src/demo/demo1/sys/start_main.js
@@ -0,0 +1,12 @@
+// (C) 2016
+'use strict';
+
+function start_main_js(){}
+
+function start_main()
+{
+    if (0) return;
+    
+    
+    wid_fill_main();
+}
---
+++ src/demo/demo1/sys/wid_fill_main.js
@@ -0,0 +1,13 @@
+// (C) 2016
+'use strict';
+
+function wid_fill_main_js(){}
+
+function wid_fill_main()
+{
+    var $cli = $('<div/>');
+
+    $g_div_main.html('');
+    start_cli($cli);
+}
+
